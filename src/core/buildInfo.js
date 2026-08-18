a124d5e749f1343d44b279fbeac44aa7692a1581
phshbone <phshbone@gmail.com>
2026-08-17 21:58:30 -0400

    Bump visible build version for trivia cache check

--- src/core/buildInfo.js
+++ src/core/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = Object.freeze({
   appName: "bill's game board",
-  version: '0.1.0',
+  version: '0.1.1',
   buildDate: '2026-08-17',
   architecture: 'PS-0.1',
-  phase: 'Sprint 1 Foundation'
+  phase: 'Trivia v1.1'
 })
