604abb48676fd829da68032e9be4bec1d664f2cd
phshbone <phshbone@gmail.com>
2026-08-17 22:10:57 -0400

    Bump build for crossword restore

--- src/core/buildInfo.js
+++ src/core/buildInfo.js
@@ -1,7 +1,7 @@
 export const BUILD_INFO = Object.freeze({
   appName: "bill's game board",
-  version: '0.1.1',
+  version: '0.1.2',
   buildDate: '2026-08-17',
   architecture: 'PS-0.1',
-  phase: 'Trivia v1.1'
+  phase: 'Crossword restore'
 })
